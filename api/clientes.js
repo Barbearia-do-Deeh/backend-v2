8f0ba23dee9a35d526c5af31b1e3652175a486cb
Barbearia-do-Deeh <davidlucas261210@gmail.com>
2026-08-10 18:29:13 -0300

    Update clientes.js

--- api/clientes.js
+++ api/clientes.js
@@ -32,7 +32,7 @@ async function renovarCicloSeVencido(client, cliente) {
 
 module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
   if (req.method === 'OPTIONS') return res.status(200).end();
